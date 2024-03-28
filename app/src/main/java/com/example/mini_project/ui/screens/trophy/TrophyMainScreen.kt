9f2e1282d313b8378055c21fa5c3d3d630bf506a
AwesomeAnna19 <annaroseyt13@gmail.com>
2024-03-28 12:15:37 +0100

    Trophy Screen pt.1 (justeringer)

--- app/src/main/java/com/example/mini_project/ui/screens/trophy/TrophyMainScreen.kt
+++ app/src/main/java/com/example/mini_project/ui/screens/trophy/TrophyMainScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
@@ -34,6 +35,11 @@ fun TrophyScreenPreview (
                 Image(
                     painter = painterResource(id = R.drawable.trophy),
                     contentDescription = toString(),
+                )
+                Text(
+                    text = "Achievement",
+                    modifier = modifier
+                        .padding(start = 20.dp, top = 130.dp)
                 )
             }
         }
