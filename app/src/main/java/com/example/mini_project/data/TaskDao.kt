f7dc0e58652687d56b7494e974105d006fb42ca6
AwesomeAnna19 <annaroseyt13@gmail.com>
2024-04-10 14:13:25 +0200

    Task DAO + Task Repository

--- app/src/main/java/com/example/mini_project/data/TaskDao.kt
+++ app/src/main/java/com/example/mini_project/data/TaskDao.kt
@@ -1,8 +1,41 @@
 package com.example.mini_project.data
 
-@DAO
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
 interface TaskDao {
 
+    /*Fra Miro: Select all based on id -> instans af klassen spyttes ud*/
+    @Query("SELECT * from tasks WHERE id = :id")
+    fun getTask(id: Int):Task
 
+    /*Fra Miro: Select og group by reminder schedule*/
+    @Query("SELECT SUM (id) from tasks GROUP BY id")
+    fun tasksReminderSchedules(id: Int):Task
+
+    /*Fra Miro: Sort based on id ascending eller titel*/
+    @Query("SELECT * from tasks ORDER BY id ASC")
+    fun groupsOfSameTasks(id: Int):Task
+
+    /*Fra Miro: filter based on category*/
+    @Query("SELECT * from tasks WHERE category = :category")
+    fun filterCategoriesStreaks(category: Category):Category
+
+    /*Fra Miro: Add*/
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Task)
+
+    /*Fra Miro: Edit*/
+    @Update
+    suspend fun update(item: Task)
+
+    /*Fra Miro: Delete*/
+    @Delete
+    suspend fun delete(item: Task)
 
 }
--- app/src/main/java/com/example/mini_project/data/TaskRepository.kt
+++ app/src/main/java/com/example/mini_project/data/TaskRepository.kt
@@ -1,6 +1,24 @@
 package com.example.mini_project.data
 
+import java.util.concurrent.Flow
+
 interface TaskRepository {
 
+    fun getTaskList(): Flow<List<Task>>
+
+    fun tasksReminderSchedulesList(): Flow<List<Task>>
+
+    fun groupsOfSameTasksList(): Flow<List<Task>>
+
+    fun filterCategoriesStreaksStats(): Flow<List<Task>>
+
+    /* Inserts/adds an item/task in the data source */
+    suspend fun insertItem(item: Task)
+
+    /* Deletes an item/task from the data source */
+    suspend fun deleteItem(item: Task)
+
+    /* Updates an item/task in the data source */
+    suspend fun updateItem(item: Task)
 
 }
