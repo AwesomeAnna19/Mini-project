42d3aa007833389e6c52b4c1da5dead483a6887c
arezva23 <151622443+arezva23@users.noreply.github.com>
2024-04-10 15:26:15 +0200

    Database and entities first draft

---
+++ app/src/main/java/com/example/mini_project/data/Badge.kt
@@ -0,0 +1,22 @@
+package com.example.mini_project.data
+
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//@Entity annotation marks a class as a database Entity class.
+// For each Entity class, the app creates a database table to hold the items.
+@Entity(tableName = "badges") // tableName argument sets badges as the SQLite table name.
+data class Badge (
+    @PrimaryKey (autoGenerate = true) //@PrimaryKey makes id the primary key to uniquely identify every entry in Item table
+    val id: Int = 0,
+    val name: String,
+    val description: String,
+
+    //Få styr på de her variabelnavne og typer!
+    val trackedStat: String,
+    val statValue: Int,
+    val maximumProgress: Int,
+
+    val isEarned: Boolean = false,
+)
--- app/src/main/java/com/example/mini_project/data/Category.kt
+++ app/src/main/java/com/example/mini_project/data/Category.kt
@@ -1,5 +1,16 @@
 package com.example.mini_project.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//@Entity annotation marks a class as a database Entity class.
+// For each Entity class, the app creates a database table to hold the items.
+@Entity(tableName = "categories") // tableName argument sets tasks as the SQLite table name.
 data class Category (
-    val name: String
+    @PrimaryKey() //@PrimaryKey makes name the primary key to uniquely identify every entry in Item table
+    val name: String,
+    val color: String,
+    val currentLevel: Int,
+    val currentXp: Int,
+    val xpRequiredForLevelUp: Int
 )
