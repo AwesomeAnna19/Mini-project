634c28b9d6190963a1b76636f1435a20b7e27422
arezva23 <151622443+arezva23@users.noreply.github.com>
2024-04-16 01:45:52 +0200

    Bottombar tilføjet til taskDetails
    scaffolden til stats skærm har jeg ikke sat op, da jeg ikke ville ændre på din skærm uden du vidste det

--- app/src/main/java/com/example/mini_project/ui/screens/home/details/TaskDetailsScreen.kt
+++ app/src/main/java/com/example/mini_project/ui/screens/home/details/TaskDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.mini_project.ui.screens.home.details
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -7,12 +8,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
 import com.example.mini_project.R
 import com.example.mini_project.data.Screen
 import com.example.mini_project.ui.AppViewModelProvider
 import com.example.mini_project.ui.navigation.NavRouteHandler
+import com.example.mini_project.ui.screens.HabitizeBottomBar
 import com.example.mini_project.ui.screens.HabitizeTopBar
 import com.example.mini_project.ui.screens.home.entry.TaskInputForm
+import com.example.mini_project.ui.screens.navItemList
 import kotlinx.coroutines.launch
 
 object TaskDetailsRoute : NavRouteHandler {
@@ -26,6 +30,7 @@ fun TaskDetailsScreen(
     navigateBack: () -> Unit,
     onNavigateUp: () -> Unit,
     modifier: Modifier = Modifier,
+    navController: NavHostController,
     viewModel: TaskDetailsViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
     val coroutineScope = rememberCoroutineScope()
@@ -36,6 +41,14 @@ fun TaskDetailsScreen(
                 title = stringResource(TaskDetailsRoute.topBarTitleResource),
                 canNavigateBack = true,
                 navigateUp = onNavigateUp
+            )
+        },
+        bottomBar = {
+            HabitizeBottomBar(
+                navItemList = navItemList,
+                navController = navController,
+                modifier = Modifier
+                    .fillMaxWidth()
             )
         },
         modifier = modifier
